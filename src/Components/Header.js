b9c8bb51dc72849d1240e285936be8654107cc79
namratanakhale <namratanakhale91@gmail.com>
2023-01-17 14:15:19 +0530

    final commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,8 @@ import { Facebook } from "@material-ui/icons";
 import { Twitter } from '@material-ui/icons';
 import { LinkedIn } from '@material-ui/icons';
 import { YouTube } from '@material-ui/icons';
+import { ArrowDropDown } from '@material-ui/icons';
+import { Menu } from '@material-ui/core';
 import odishalogo from '../images/odishalogo.png';
 import newormaslogo1 from '../images/newormaslogo1.png';
 import cm_on_nav from '../images/cm_on_nav.png';
@@ -36,7 +38,7 @@ function Header() {
                    
                 </div>
 
-                <img src={odishalogo} style={{paddingTop:"7%", height:"100px", width:"152px"}} /> 
+                <img src={odishalogo} className="odishalogo" /> 
                 <div style={{ display: "flex",
             justifyContent: "center",
             flexDirection: "column",
@@ -66,36 +68,37 @@ function Header() {
 
         </span>
 
-        <nav className="" role="navigation" 
-      style={{display: "flex", paddingLeft: "25%", backgroundColor:"#009d00", marginTop:"-4px", height:"43px"}}>
-      <div className="nav-wrapper container" style={{margin: "0px"}}>
-        <ul className="center hide-on-med-and-down" >
-          <li className="tab white-text"
+      <nav className="navbar" role="navigation" >
+      <div className="nav-wrapper">
+        <ul className="main-container" >
+          <li className="white-text"
             style={{borderLeft: "1px solid #14520b", paddingLeft:"2px", paddingRight:"2px",height:"43px"}}>
-            <a href="#test1" className="white-text">Home</a>
+            <div className='home'><a href="#test1" className="white-text" style={{outline:"0"}}>Home</a></div>
           </li>
           <li style={{borderLeft: "1px solid #14520b", height:"43px"}}>
-            <a className="dropdown-trigger white-text" href="#!" data-target="aboutUsDrop">About Us<i
-                className="material-icons right">arrow_drop_down</i></a>
+            <a className="white-text" href="#!" data-target="aboutUsDrop">About Us 
+            <ArrowDropDown />
+            </a>
           </li>
           <li style={{borderLeft: "1px solid #14520b",height:"43px"}}>
-            <a className="dropdown-trigger white-text" href="#!" data-target="dduGkyDrop">DDU-GKY<i
-                className="material-icons right">arrow_drop_down</i></a>
-          </li>
-          <li className="tab white-text" style={{borderLeft: "1px solid #14520b",height:"43px"}}>
-            <a href="#test1" className="white-text">Activities</a>
+            <a className="white-text" href="#!" data-target="dduGkyDrop">DDU-GKY
+            <ArrowDropDown /></a>
           </li>
           <li style={{borderLeft: "1px solid #14520b",height:"43px"}}>
-            <a className="dropdown-trigger white-text" href="#!" data-target="productsDrop">Products<i
-                className="material-icons right">arrow_drop_down</i></a>
+            <a href="#test1" className="white-text">Activities
+            <ArrowDropDown /></a>
           </li>
           <li style={{borderLeft: "1px solid #14520b",height:"43px"}}>
-            <a className="dropdown-trigger white-text" href="#!" data-target="exhibitionDrop">Exhibition<i
-                className="material-icons right">arrow_drop_down</i></a>
+            <a className="white-text" href="#!" data-target="productsDrop">Products
+            <ArrowDropDown /></a>
+          </li>
+          <li style={{borderLeft: "1px solid #14520b",height:"43px"}}>
+            <a className="white-text" href="#!" data-target="exhibitionDrop">Exhibition
+            <ArrowDropDown /></a>
           </li>
           <li style={{borderLeft: "1px solid #14520b",borderRight: "1px solid #14520b",height:"43px"}}
             class="tab white-text">
-            <a href="#test1" className="white-text">Tender</a>
+            <div className='home'><a href="#test1" className="white-text">Tender</a></div>
           </li>
         </ul>
         {/* <ul id="nav-mobile" className="sidenav" style={{display:"flex"}}>
@@ -108,8 +111,8 @@ function Header() {
           <li><a href="#">Tender</a></li>
 
         </ul> */}
-        <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons"
-            style={{color: "#ffffff", marginLeft: "-100px", marginTop: "-5px"}}>menu</i></a>
+        <a href="#" data-target="nav-mobile" className="sidenav-trigger"
+            style={{color: "#ffffff", marginLeft: "-100px", marginTop: "-5px"}}><Menu /></a>
       </div>
     </nav>
 
